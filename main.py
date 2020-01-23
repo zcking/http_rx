b155821e3db70f26334e763ff3a417c7dc7f2f65
Zachary King <kingzach77@gmail.com>
2020-01-22 21:15:45 -0600

    refactored files so app can be run from external sources

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-"""
-main.py
-
-Entry point for running the http_rx as
-a script, or program.
-"""
-
-import time
-import logging
-
-from rx import doctor
-
-def main():
-    import json
-    import os
-
-    # Parse JSON config file
-    config_file_path = os.environ.get('RX_CONF', 'config.json')
-    with open(config_file_path) as config_file:
-        config = json.load(config_file)
-
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
-    doc = doctor.Doctor(config)
-
-    while True:
-        health_report = doc.run()
-        logging.info(str(health_report))
-        health_report.log_failures()
-        time.sleep(config.get('intervalSeconds', 5)) # sleep for 5 seconds by default
-
+import rx
 
 if __name__ == '__main__':
-    main()
-
+    rx.run()
--- rx/__init__.py
+++ rx/__init__.py
@@ -1,1 +1,27 @@
-from . import check, doctor
+from . import check, doctor, report
+
+import time
+import logging
+
+
+def run():
+    """
+    Run the HTTP RX application.
+    """
+    import json
+    import os
+
+    # Parse JSON config file
+    config_file_path = os.environ.get('RX_CONF', 'config.json')
+    with open(config_file_path) as config_file:
+        config = json.load(config_file)
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
+    doc = doctor.Doctor(config)
+
+    while True:
+        health_report = doc.run()
+        logging.info(str(health_report))
+        health_report.log_failures()
+        time.sleep(config.get('intervalSeconds', 5)) # sleep for 5 seconds by default
+
