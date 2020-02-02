9e87394f19d2c3e23db9f1c301e18b7bcc463187
Zachary King <kingzach77@gmail.com>
2020-02-02 11:26:14 -0600

    version 0.4.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as rf:
 
 setuptools.setup(
     name='http_rx',
-    version='0.3.1',
+    version='0.4.1',
     author='Zachary King',
     author_email='kingzach77@gmail.com',
     description='Extensible HTTP health checker for Python',
